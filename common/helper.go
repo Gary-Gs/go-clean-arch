7120172963379c4f73eb9298c6eff90d01257c1a
Gary <gary.guo@envision-digital.com>
2022-12-14 12:19:07 +0800

    update helper functions and go mod

--- common/helper.go
+++ common/helper.go
@@ -1,6 +1,8 @@
 package common
 
-import "strings"
+import (
+	"strings"
+)
 
 func ContainsIgnoreCase(arr []string, target string) bool {
 	target = strings.ToUpper(target)
@@ -10,4 +12,70 @@ func ContainsIgnoreCase(arr []string, target string) bool {
 		}
 	}
 	return false
+}
+
+// Min returns the minimum value in a list of values.
+func Min(values ...interface{}) float64 {
+	minValue := values[0].(float64)
+	for _, value := range values {
+		switch value := value.(type) {
+		case int64:
+			if value < int64(minValue) {
+				minValue = float64(value)
+			}
+		case float64:
+			if value < minValue {
+				minValue = value
+			}
+		case int:
+			// Handle float64 values
+			if value < int(minValue) {
+				minValue = float64(value)
+			}
+		default:
+			// ignore other types
+		}
+	}
+	return minValue
+}
+
+// Max returns the maximum value in a list of values.
+func Max(values ...interface{}) float64 {
+	maxValue := values[0].(float64)
+	for _, value := range values {
+		switch value := value.(type) {
+		case int64:
+			if value > int64(maxValue) {
+				maxValue = float64(value)
+			}
+		case float64:
+			if value > maxValue {
+				maxValue = value
+			}
+		case int:
+			if value > int(maxValue) {
+				maxValue = float64(value)
+			}
+		default:
+			// ignore other types
+		}
+	}
+	return maxValue
+}
+
+// RemoveElementByIndex removes an element from a slice by index.
+func RemoveElementByIndex[T any](slice []T, index int) []T {
+	// check if index is out of range
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
+
+// InsertElementByIndex inserts an element to a slice by index.
+func InsertElementByIndex[T any](slice []T, index int, element T) []T {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], append([]T{element}, slice[index:]...)...)
 }
--- common/helper_test.go
+++ common/helper_test.go
@@ -16,3 +16,37 @@ func TestContainsIgnoreCase(t *testing.T) {
 
 	assert.Equal(t, true, ContainsIgnoreCase([]string{"a", "b", "c"}, "A"))
 }
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, -3.2, Min(-3.2, 0, 1.6))
+}
+
+func TestMax(t *testing.T) {
+	assert.Equal(t, float64(8), Max(-3.2, 0, 8, 1.6))
+}
+
+func TestRemoveElementByIndex(t *testing.T) {
+	// test string
+	assert.Equal(t, []string{"a", "c"}, RemoveElementByIndex([]string{"a", "b", "c"}, 1))
+	// test int
+	assert.Equal(t, []int{1, 3}, RemoveElementByIndex([]int{1, 2, 3}, 1))
+	// test float64
+	assert.Equal(t, []float64{1.1, 3.3}, RemoveElementByIndex([]float64{1.1, 2.2, 3.3}, 1))
+	// test out of bound index
+	assert.Equal(t, []string{"a", "b", "c"}, RemoveElementByIndex([]string{"a", "b", "c"}, 3))
+	// test negative range bound
+	assert.Equal(t, []string{"a", "b", "c"}, RemoveElementByIndex([]string{"a", "b", "c"}, -1))
+}
+
+func TestInsertElementByIndex(t *testing.T) {
+	// test string
+	assert.Equal(t, []string{"a", "d", "b", "c"}, InsertElementByIndex([]string{"a", "b", "c"}, 1, "d"))
+	// test int
+	assert.Equal(t, []int{1, 4, 2, 3}, InsertElementByIndex([]int{1, 2, 3}, 1, 4))
+	// test float64
+	assert.Equal(t, []float64{1.1, 4.4, 2.2, 3.3}, InsertElementByIndex([]float64{1.1, 2.2, 3.3}, 1, 4.4))
+	// test out of bound index
+	assert.Equal(t, []string{"a", "b", "c"}, InsertElementByIndex([]string{"a", "b", "c"}, 4, "d"))
+	// test negative range bound
+	assert.Equal(t, []string{"a", "b", "c"}, InsertElementByIndex([]string{"a", "b", "c"}, -1, "d"))
+}
